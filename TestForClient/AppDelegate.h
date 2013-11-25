3327bd7b75808dc482c992986a9f9d7225790f38
Ramanathan Uthirapathy <ram@MacBookPro.local>
2013-11-25 13:26:53 +0530

    Initial Commit

---
+++ TestForClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestForClient
+//
+//  Created by Ramanathan Uthirapathy on 25/11/13.
+//  Copyright (c) 2013 Ramanathan Uthirapathy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestForClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestForClient
+//
+//  Created by Ramanathan Uthirapathy on 25/11/13.
+//  Copyright (c) 2013 Ramanathan Uthirapathy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
